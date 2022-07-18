76a0b49097dc47d23c89e1078b3b8d288675bdce
Tom <tom.ravaud@eleves.enpc.fr>
2022-07-18 14:53:28 +0200

    Added a node which sets a sinusoidal position input, tried to tune the PIDs

---
+++ mobile_platform/platform_control/command/sinusoid_pos.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python3
+
+import rospy
+import numpy as np
+from std_msgs.msg import Float64
+
+rospy.init_node("sinusoid_pos")
+
+# The queue_size argument allows to synchronise publisher and subscriber
+# frequencies
+pub_roll = rospy.Publisher("/mobile_platform/roll_joint_position_controller/command",
+                      Float64, queue_size=1)
+# pub_pitch = rospy.Publisher("/mobile_platform/pitch_joint_position_controller/command",
+#                       Float64, queue_size=1)
+
+# Rate at which messages are published on the topic 
+publishing_rate = 100
+rate = rospy.Rate(publishing_rate)
+
+# Instantiate a Float64 object to store the position value that will be sent
+# to the platform
+position = Float64()
+
+# Set the amplitude (radians) and the frequency (Hz) of the sinusoid
+amplitude = 20*np.pi/180
+frequency = 0.5
+
+# time = rospy.get_time()
+
+while not rospy.is_shutdown():
+    position.data = amplitude*np.cos(2*np.pi*frequency*(rospy.get_time()))
+    # position.data = amplitude*np.cos(2*np.pi*frequency*(rospy.get_time()-time))
+    
+    # Publish the position
+    pub_roll.publish(position)
+    # pub_pitch.publish(position)
+     
+    rate.sleep()
